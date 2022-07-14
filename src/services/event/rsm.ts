35c52dad95769238a3474766f666ee0c6ebf494d
tangyin <tangyin@donglan.tech>
2022-07-14 18:12:12 +0800

    fix: change the API interface address of RSM
    Issue: 141308
    Change-Id: Ic4a7da60550e807d7497d507da7b6a733cd2d729

--- src/services/event/rsm.ts
+++ src/services/event/rsm.ts
@@ -2,7 +2,7 @@ import request from '../request';
 
 // 事件信息列表
 export async function roadSideMessageList(params: API.PageParams) {
-  return request<API.ListResult<Event.RSMListItem>>(`/rsms`, {
+  return request<API.ListResult<Event.RSMListItem>>(`/v1/rsms`, {
     method: 'GET',
     params,
   });
