758da8f565cfbb42456e56c0698709e5f5fb9838
Zhangke <100893916+Zhangke01@users.noreply.github.com>
2022-11-07 14:37:44 +0800

    feat: upgrade playwright version and add e2e test example (#78)
    * feat: upgrade playwright version and add e2e test example
    
    1. upgrade playwright to latest version(from 1.17.0 to 1.27.1)
    2. update playwright.config.ts
    3. add e2e test example
    4. add playwright.yml for github workflows

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,22 +1,122 @@
-﻿// playwright.config.ts
 import type { PlaywrightTestConfig } from '@playwright/test';
 import { devices } from '@playwright/test';
 
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+// require('dotenv').config();
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
 const config: PlaywrightTestConfig = {
-  forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  use: {
-    trace: 'on-first-retry',
+  testDir: './e2e',
+  /* Maximum time one test can run for. */
+  timeout: 30 * 10000,
+  expect: {
+    /**
+     * Maximum time expect() should wait for the condition to be met.
+     * For example in `await expect(locator).toHaveText();`
+     */
+    timeout: 5000,
   },
+  /* Run tests in files in parallel */
+  fullyParallel: true,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: !!process.env.CI,
+  /* Retry on CI only */
+  retries: process.env.CI ? 2 : 0,
+  /* Opt out of parallel tests on CI. */
+  workers: process.env.CI ? 1 : undefined,
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: 'html',
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  use: {
+    /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
+    actionTimeout: 0,
+    /* Base URL to use in actions like `await page.goto('/')`. */
+    // baseURL: 'http://localhost:3000',
+    /* browserName - Name of the browser that will run the tests, one of chromium, firefox, or webkit. */
+    browserName: 'chromium',
+
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    trace: 'on-first-retry',
+    /**
+     * 'off' - Do not capture screenshots.
+     * 'on' - Capture screenshot after each test.
+     * 'only-on-failure' - Capture screenshot after each test failure.
+     */
+    screenshot: 'only-on-failure',
+    /**
+     * 'off' - Do not record video.
+     * 'on' - Record video for each test.
+     * 'retain-on-failure' - Record video for each test, but remove all videos from successful test runs.
+     * 'on-first-retry' - Record video only when retrying a test for the first time.
+     */
+    video: 'retain-on-failure',
+  },
+
+  /* Configure projects for major browsers */
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      use: {
+        ...devices['Desktop Chrome'],
+      },
     },
+
     {
       name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
+      use: {
+        ...devices['Desktop Firefox'],
+      },
     },
+
+    {
+      name: 'webkit',
+      use: {
+        ...devices['Desktop Safari'],
+      },
+    },
+
+    /* Test against mobile viewports. */
+    // {
+    //   name: 'Mobile Chrome',
+    //   use: {
+    //     ...devices['Pixel 5'],
+    //   },
+    // },
+    // {
+    //   name: 'Mobile Safari',
+    //   use: {
+    //     ...devices['iPhone 12'],
+    //   },
+    // },
+
+    /* Test against branded browsers. */
+    // {
+    //   name: 'Microsoft Edge',
+    //   use: {
+    //     channel: 'msedge',
+    //   },
+    // },
+    // {
+    //   name: 'Google Chrome',
+    //   use: {
+    //     channel: 'chrome',
+    //   },
+    // },
   ],
+
+  /* Folder for test artifacts such as screenshots, videos, traces, etc. */
+  // outputDir: 'test-results/',
+
+  /* Run your local dev server before starting the tests */
+  // webServer: {
+  //   command: 'npm run start',
+  //   port: 3000,
+  // },
 };
+
 export default config;
