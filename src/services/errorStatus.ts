8468a6c0cc401cace0fdfc0195bc6ae995e6c006
liyoumin <li.youmin@99cloud.net>
2022-11-16 16:42:25 +0800

    fix: fix the error 1116

--- src/services/errorStatus.ts
+++ src/services/errorStatus.ts
@@ -1,13 +1,16 @@
 import { message } from 'antd';
 
-export default function errorStatus(code: number | undefined, msg: string) {
-  try {
-    if (code) {
+export default function errorStatus(code: number | undefined, msg: string, detail: any) {
+  switch (code) {
+    case 1062:
+    case 1406:
       return message.error(t(`error.${code}`, { msg: msg }));
-    } else {
+    case 1116:
+      const { intersection_id, phase_id } = detail;
+      return message.error(
+        t(`error.${code}`, { intersectionId: intersection_id, phaseId: phase_id }),
+      );
+    default:
       return message.error(msg);
-    }
-  } catch (error) {
-    return message.error(error);
   }
 }
